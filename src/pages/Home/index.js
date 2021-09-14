133e032a90bc73fb703b0c3d5058f35055825595
Guilhermano Silva <guilhermanodev@gmail.com>
2021-09-14 14:02:36 -0300

    initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          href="https://github.com/guilhermanosilva"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Guilhermano Silva
+        </a>
+      </header>
+    </div>
+  );
+}
