e62d83ce59f5f9d87d48cc9a4885c2dbf4f96cf0
Guilhermano Silva <guilhermanodev@gmail.com>
2021-09-28 21:32:27 -0300

    style: remove comments in sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,3 @@
-// /* eslint-disable no-restricted-globals */
-// /* eslint-disable array-callback-return */
-// /* eslint-disable consistent-return */
-// /* eslint-disable no-console */
-// /* eslint-disable no-var */
-
-// self.addEventListener('active', (event) => {
-//   var cacheWhiteList = ['pwa-task-manager'];
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => Promise.all(
-//       cacheNames.map((cacheName) => {
-//         if (cacheWhiteList.indexOf(cacheName) === -1) {
-//           return caches.delete(cacheName);
-//         }
-//       }),
-//     )),
-//   );
-// });
-
 /* eslint-disable */
 const CACHE_NAME = 'pwa-guilhermano-silva';
 const urlsToCache = [
@@ -41,8 +22,6 @@ self.addEventListener('activate', (event) => {
     })))
   )
 });
-
-// self.skipWaiting();
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(caches.open(CACHE_NAME)
